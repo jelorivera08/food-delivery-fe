ad726fcabeada25681b18fe832c657c1c4c8acc6
Jerald Rivera <jlrivera@gofluent.com>
2019-06-28 15:36:25 +0800

    added favicon

--- src/App.js
+++ src/App.js
@@ -127,6 +127,8 @@ function App() {
         open: true,
         status: '',
       });
+
+      getMenu();
     } catch (err) {
       setOpenSnackbar({
         message: 'I already know you! log in.',
