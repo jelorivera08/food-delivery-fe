936ae74553925af74807b986973d6b21afb86942
Jerald Rivera <jlrivera@gofluent.com>
2019-06-30 14:47:03 +0800

    menu ui done

--- src/App/components/Dashboard/Menu.js
+++ src/App/components/Dashboard/Menu.js
@@ -3,7 +3,23 @@ import React from 'react';
 const UserProfile = () => {
   return (
     <div className="menu-container">
-      <div className="menu"> menu</div>
+      <div className="menu">
+        <div className="menu-header">
+          <span className="menu-header-text">{`Today's menu. 😋`}</span>
+        </div>
+
+        <div className="menu-item-container">
+          <div className="menu-item">
+            <div className="menu-item-text">Adobo for 50 PHP</div>
+          </div>
+          <div className="menu-item">
+            <div className="menu-item-text">Menudo for 15 PHP</div>
+          </div>
+          <div className="menu-item">
+            <div className="menu-item-text">Togue for 20 PHP</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
