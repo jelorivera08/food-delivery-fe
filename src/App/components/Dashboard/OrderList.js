830677393fc535da0ed6572e693aefbee28b68fa
Jerald Rivera <jlrivera@gofluent.com>
2019-07-03 09:04:51 +0800

    cut off functionality added

--- src/App/components/Dashboard/OrderList.js
+++ src/App/components/Dashboard/OrderList.js
@@ -5,6 +5,21 @@ const OrderList = ({ deleteOrder, orders }) => {
   const handleDeleteClick = (id) => () => {
     deleteOrder(id);
   };
+
+  const checkIfCutoff = () => {
+    let time = new Date().toLocaleTimeString();
+
+    let timeSplit = time.split(':');
+
+    if (timeSplit[0] === '11') {
+      return true;
+    }
+
+    return false;
+  };
+
+  const isCutoff = checkIfCutoff();
+
   return (
     <div className="order-list-container">
       {orders.length <= 0 && (
@@ -20,11 +35,13 @@ const OrderList = ({ deleteOrder, orders }) => {
               {`• ${order.quantity} order of ${order.name} for ${order.price *
                 order.quantity} PHP`}
             </div>
-            <div className="order-item-icon">
-              <DeleteForeverOutlinedIcon
-                onClick={handleDeleteClick(order._id)}
-              />
-            </div>
+            {!isCutoff && (
+              <div className="order-item-icon">
+                <DeleteForeverOutlinedIcon
+                  onClick={handleDeleteClick(order._id)}
+                />
+              </div>
+            )}
           </div>
         );
       })}
