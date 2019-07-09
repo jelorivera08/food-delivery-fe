d6c097f1b9c1d05807b9e0d68d04d64d6264da61
Jerald Rivera <jlrivera@gofluent.com>
2019-07-09 21:39:36 +0800

    change url depending on env

--- src/App/constants/globalConstants.js
+++ src/App/constants/globalConstants.js
@@ -1,5 +1,7 @@
-export const URL = 'https://grabjunjun.herokuapp.com/api/v1';
-// export const URL = 'http://localhost:3000/api/v1';
+export const URL =
+  !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/api/v1'
+    : 'https://grabjunjun.herokuapp.com/api/v1';
 
 export const ERROR = 'ERROR';
 export const ADMIN = 'admin';
