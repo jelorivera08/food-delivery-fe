ba1451db4a5b2422abafc2262f33b991f90395e1
Jerald Rivera <jlrivera@gofluent.com>
2019-07-26 15:29:34 +0800

    adaptive url exporting

--- src/App/constants/globalConstants.js
+++ src/App/constants/globalConstants.js
@@ -1,4 +1,8 @@
-export const URL = 'https://grabjunjun.herokuapp.com/api/v1';
+// export const URL = 'https://grabjunjun.herokuapp.com/api/v1';
 // export const URL = 'http://localhost:3000/api/v1';
+export const URL =
+  process.env._ && process.env._.indexOf('heroku')
+    ? 'https://grabjunjun.herokuapp.com/api/v1'
+    : 'http://localhost:3000/api/v1';
 export const ERROR = 'ERROR';
 export const ADMIN = 'admin';
