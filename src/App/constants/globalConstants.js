662bdc0c87b1e894cfb80f6895a757dfc5a53e50
Jerald Rivera <jlrivera@gofluent.com>
2019-07-26 15:35:25 +0800

    change url

--- src/App/constants/globalConstants.js
+++ src/App/constants/globalConstants.js
@@ -1,8 +1,4 @@
-// export const URL = 'https://grabjunjun.herokuapp.com/api/v1';
+export const URL = 'https://grabjunjun.herokuapp.com/api/v1';
 // export const URL = 'http://localhost:3000/api/v1';
-export const URL =
-  process.env._ && process.env._.indexOf('heroku')
-    ? 'https://grabjunjun.herokuapp.com/api/v1'
-    : 'http://localhost:3000/api/v1';
 export const ERROR = 'ERROR';
 export const ADMIN = 'admin';
